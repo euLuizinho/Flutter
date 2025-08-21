01602280f0c9ff763c67ae53d84622543683691f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-21 20:53:48 -0300

    first commit

---
+++ lib/exibicao_clientes/exibicao_cliente.screen.dart
@@ -0,0 +1,44 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class ExibicaoClienteScreen extends StatefulWidget {
+  final String title;
+
+  const ExibicaoClienteScreen({super.key, required this.title});
+
+  @override
+  State<ExibicaoClienteScreen> createState() => _ExibicaoClienteScreenState();
+}
+
+class _ExibicaoClienteScreenState extends State<ExibicaoClienteScreen> {
+   List<Map<String, dynamic>> clientes =[];
+
+  @override
+  void initState() {
+    clientes.add({"nome":"Luizinho", "idade":21});
+    clientes.add({"nome":"Busch", "idade":40});
+    clientes.add({"nome":"Never", "idade":6});
+    super.initState();
+  }
+
+  Widget formatarTexto (String nome, int idade){
+    String eMaior = idade>=18 ? "é de maior" : "é de menor";
+
+    return Text("Cliente $nome $eMaior");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title),),
+      body: Column(
+        children: [
+          Text("Clientes"),
+          for(var cliente in clientes)
+            formatarTexto(cliente['nome'], cliente['idade'])
+        ],
+      )
+    );
+  }
+}
---
+++ lib/exibicao_clientes/gestao_pedidos.screen.dart
@@ -0,0 +1,30 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class GestaoPedidosScreen extends StatefulWidget {
+  final String title;
+
+  const GestaoPedidosScreen({super.key, required this.title});
+
+  @override
+  State<GestaoPedidosScreen> createState() => _GestaoPedidosScreenState();
+}
+
+class _GestaoPedidosScreenState extends State<GestaoPedidosScreen> {
+   
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title),),
+      body: Column(
+        children: [
+          Text("Gestão Pedidos"),
+         
+        ],
+      )
+    );
+  }
+}
